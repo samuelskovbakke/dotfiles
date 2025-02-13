305826022f7c6bef310cc6eb48dd7e27c7000c87
Samuel Skovbakke <samuel@skovbakke.dk>
2025-02-13 16:34:09 +0100

    Added .icons folder, waybar and openrgb config

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ sudo pacman -Syyu
 sudo pacman -S --needed base-devel
 
 sudo pacman -S zsh ghostty fastfetch git neovim neovide tmux stow fzf zoxide ripgrep bat thefuck eza zip unzip unrar xarchiver xdg-utils \
-celluloid okular fd htop tldr wget lshw thunar  \
+celluloid okular fd htop tldr wget lshw thunar thunar-archive-plugin \
 cmake gcc rust ruby python python-pip jdk-openjdk npm luarocks \
 font-manager ttf-firacode-nerd ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra wqy-zenhei \
 waybar hyprpaper hyprlock swaync pipewire pipewire-pulse swappy grim wl-clipboard slurp wofi network-manager-applet wtype wl-mirror \
-firefox
+firefox openrgb
 
 # Install yay and AUR packages
 git clone https://aur.archlinux.org/yay.git ~
