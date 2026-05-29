49f2f2584ebe2774b41cc69a08e620c841245bfe
samuelskovbakke <samuel@skovbakke.dk>
2026-05-29 22:24:40 +0200

    added fish dots

---
+++ .config/fish/functions/my-aliases.fish
@@ -0,0 +1,32 @@
+# General stuff
+alias q='exit'
+alias c='clear'
+alias cat='bat'
+alias ls="eza --color=always --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias la="eza --long --all --color=always --git --icons=always --no-time --no-user"
+alias l="eza --long --color=always --git --no-filesize --icons=always --no-time --no-user"
+alias more='most'
+alias pac='sudo pacman --noconfirm'
+alias yay='yay --noconfirm'
+alias give-me-upgrade='sudo pacman --noconfirm -Syyuu && yay'
+alias grep='rg'
+alias ff='fastfetch'
+alias gg='lazygit'
+
+# Nix
+abbr -a nrs 'sudo nixos-rebuild switch --impure --flake ~/nix-config && home-manager switch --impure --flake ~/nix-config'
+abbr -a nixupg 'nix flake update --flake ~/nix-config && home-manager switch --impure --flake ~/nix-config && sudo nixos-rebuild switch --impure --flake ~/nix-config'
+abbr -a nixdelgens 'sudo nix-collect-garbage -d && nix-collect-garbage -d'
+
+# Directory shortcuts
+abbr -a md 'mkdir -p'
+abbr -a rd 'rmdir'
+
+# Directory history viewer
+function d
+    if set -q argv[1]
+        dirh
+    else
+        dirh | head -n 10
+    end
+end
